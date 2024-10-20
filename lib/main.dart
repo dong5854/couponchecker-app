2dc57be7b1ff1c4eddfcf56e4538c58fe3db13ff
dong young <dong5854@gmail.com>
2024-10-20 23:33:31 +0900

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:couponchecker/screen/coupon_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -12,13 +21,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: const Color.fromARGB(255, 143, 230, 195), useMaterial3: true),
-      home: const CouponChecker(),
+      theme: ThemeData(colorSchemeSeed: const Color.fromARGB(255, 143, 230, 195), useMaterial3: true),
+      initialRoute: FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/',
+        routes: {
+          '/' : (context) => const CouponChecker(),
+          '/sign-in' : (context) => SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+          )
+        },
       localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate, // Add this line
-        GlobalWidgetsLocalizations.delegate, // Add this line
-        GlobalCupertinoLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
         Locale('en'),
