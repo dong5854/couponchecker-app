ff5a6a7301751cd858c237feb92595e55c65f9f0
dong young <dong5854@gmail.com>
2024-10-20 21:24:41 +0900

    feat: show used_coupon_list

--- lib/view/used_coupon_list_view.dart
+++ lib/view/used_coupon_list_view.dart
@@ -1,4 +1,7 @@
+import 'package:couponchecker/widget/coupon_card.dart';
+import 'package:couponchecker/widget/image_dialog.dart';
 import 'package:flutter/material.dart';
+
 class UsedCouponListView extends StatelessWidget {
   const UsedCouponListView({super.key});
 
@@ -6,19 +9,27 @@ class UsedCouponListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Map<String, String>> items = [
       {'imageUrl': 'https://images.pokemoncard.io/images/cel25/cel25-7.png', 'title': '날으는 뚱카츄'},
+      {'imageUrl': 'https://images.pokemoncard.io/images/base2/base2-60.png', 'title': '근본 뚱카츄'},
+      {'imageUrl': 'https://images.pokemoncard.io/images/cel25/cel25-9.png', 'title': '서핑 뚱카츄'},
+      {'imageUrl': 'https://images.pokemoncard.io/images/swsh4/swsh4-188.png', 'title': '무지개 뚱카츄'},
     ];
 
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
-    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-
-    return ListView.builder(
+        return ListView.builder(
       itemCount: 25,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Image.network(items[0]['imageUrl']!),
-          tileColor: index.isOdd ? oddItemColor : evenItemColor,
-          title: Text('사용완료 $index'),
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return ImageDialog(imageUrl: items[index % 4]['imageUrl']!);
+              },
+            );
+          },
+          child: CouponCard(
+            imageUrl: items[index % 4]['imageUrl']!,
+            title: items[index % 4]['title']!,
+          ),
         );
       },
     );
